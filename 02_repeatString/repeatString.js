2be51672fcb0d5a85afdbe8cbd032a7ef92a1d63
arya askarpour <ayaska@pm.me>
2023-08-14 12:44:21 +0100

    compleye first 2 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, reps) {
+    if (reps < 0) {
+        return 'ERROR'
+    }
+    
+    let output = ""
+    for (let i = 0; i < reps; i++) {
+        output += string
+    }
+    return output
 };
 
 // Do not edit below this line
